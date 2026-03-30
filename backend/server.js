dc732f0e084ef67967dd3b6a6cccd85e6dbddf27
sameer-a12 <sameerali06567@gmail.com>
2026-03-30 11:12:51 +0530

    Update server.js file

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,28 @@ import cors from 'cors';
 const app = express();
 const port = process.env.PORT || 4000;
 
+const allowedOrigins = [
+  "http://localhost:5173", 
+  "http://localhost:5174",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      return callback(new Error("Not allowed by CORS"));
+    },
+    credentials: true, 
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 import express from 'express';
 app.use(express.json());
 
@@ -14,6 +36,7 @@ import { clerkMiddleware } from "@clerk/express";
 import doctorRouter from './routes/doctorRouter.js';
 import serviceRouter from './routes/serviceRouter.js';
 import appointmentRouter from './routes/appointmentRouter.js';
+import serviceAppointmentRouter from './routes/serviceAppointmentRouter.js';
 
 
 
@@ -22,6 +45,7 @@ app.use(express.urlencoded({ limit: "20mb", extended: true }));
 app.use("/api/doctors", doctorRouter);
 app.use("/api/services",serviceRouter);
 app.use("/api/appointments",appointmentRouter);
+app.use("api/services-appointments",serviceAppointmentRouter);
 
 app.get('/', (req, res) => {
     res.send('API Working ');
