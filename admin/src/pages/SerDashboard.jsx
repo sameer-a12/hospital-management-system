52ce6cfda38196f1650025b30db5724985592a0d
sameer-a12 <sameerali06567@gmail.com>
2026-04-02 10:04:01 +0530

    feat: add service dashboard components and pages

---
+++ admin/src/pages/SerDashboard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import ServiceDashboard from '../components/ServiceDashboard'
+
+const SerDashboard = () => {
+  return (
+    <div>
+        
+        <ServiceDashboard/>
+    </div>
+  )
+}
+
+export default SerDashboard
