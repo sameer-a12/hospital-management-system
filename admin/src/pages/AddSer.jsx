4b21ed635e6403a077b8220ff102c66318a81d1c
sameer-a12 <sameerali06567@gmail.com>
2026-04-03 11:37:41 +0530

    feat: add service creation page

---
+++ admin/src/pages/AddSer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import AddService from '../components/AddService';
+const AddSer = () => {
+  return (
+    <div>
+        <AddService/>
+    </div>
+  )
+}
+
+export default AddSer
