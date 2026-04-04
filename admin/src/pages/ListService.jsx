e9121f72e4f476f4e709dffdfd6679a0eb9a6346
sameer-a12 <sameerali06567@gmail.com>
2026-04-04 11:40:34 +0530

    feat: add service list page with editing and slot management

---
+++ admin/src/pages/ListService.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import ListServicePage from '../components/ListServicePage'
+const ListService = () => {
+  return (
+    <div>
+        <ListServicePage/>
+    </div>
+  )
+}
+
+export default ListService
