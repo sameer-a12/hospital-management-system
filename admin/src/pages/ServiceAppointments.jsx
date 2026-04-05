f880be03634e9c4fb88ab760d76f631d7e72fac6
sameer-a12 <sameerali06567@gmail.com>
2026-04-05 09:51:44 +0530

    Add ServiceAppointmentsPage with search, filter, status update, and rescheduling

---
+++ admin/src/pages/ServiceAppointments.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ServiceAppointmentsPage from '../components/ServiceAppointmentsPage'
+const ServiceAppointments = () => {
+  return (
+    <div>
+      
+      <ServiceAppointmentsPage/>
+    </div>
+  )
+}
+export default ServiceAppointments
