6e597ef955e9ef5503c6978951d4a4063b2527d2
sameer-a12 <sameerali06567@gmail.com>
2026-04-19 17:03:01 +0530

    Add doctor edit profile page

---
+++ frontend/src/doctor/EditProfile.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import EditProfilePage from '../components/EditProfilePage'
+import DoctorNavbar from "../doctor/DoctorNavbar"
+const EditProfile = () => {
+  return (
+    <div>
+        
+        <DoctorNavbar/>
+        <EditProfilePage/>
+    </div>
+  )
+}
+
+export default EditProfile
